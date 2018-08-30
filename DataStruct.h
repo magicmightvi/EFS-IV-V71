d29a7dffb09c51aa01774d10e9865be13d062f41
magicmightvi <taohaerbin@sina.com>
2018-08-30 15:07:18 +0800

    8.30-1
    v75

--- DataStruct.h
+++ DataStruct.h
@@ -18,8 +18,8 @@ extern "C"
 //	#include "SMS\PrtcSms.h"
      
 // #pragma location=0x0FF00 
- const unsigned char VerNum[]="Ver99.00";
-const unsigned char VerNum_INLCD=99;
+ const unsigned char VerNum[]="Ver75.00";
+const unsigned char VerNum_INLCD=75;
 //================================== LOG日志模块 ============================================ 
 struct sLOG_DATA//1条LOG日志的内容
 {
